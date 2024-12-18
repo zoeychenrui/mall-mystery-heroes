a2f8eea0b6aac94dc50dcb3e2af31bffa20ea57e
SPGTofu <j243982p@gmail.com>
2024-12-18 17:11:30 -0600

    actually removed storage call test

--- src/components/storageCalls.js
+++ src/components/storageCalls.js
@@ -1,11 +1,11 @@
 import { ref, uploadBytes } from "firebase/storage";
 import { storage } from "../utils/firebase"
 
-export const testStorageCall = async () => {
-    const storageRef = ref(storage, 'test');
-    const file = new Blob(['Hello World']);
-    console.log('running');
-    await uploadBytes(storageRef, file).then((snapshot) => {
-        console.log('uloaded file: ', snapshot);
-    });
-}
+// export const testStorageCall = async () => {
+//     const storageRef = ref(storage, 'test');
+//     const file = new Blob(['Hello World']);
+//     console.log('running');
+//     await uploadBytes(storageRef, file).then((snapshot) => {
+//         console.log('uloaded file: ', snapshot);
+//     });
+// }
